1213b6c4bfbaf83d05f086cd7034123ab1473650
anais <anais.code@proton.me>
2024-08-30 19:21:10 +0200

    add : trackUser function with location

--- srcs/trackLocation.js
+++ srcs/trackLocation.js
@@ -3,17 +3,30 @@ const {Api42} = require('../Api42.js/Api42.js');
 const api42 = new Api42();
 
 const tracking = [
-	{login: "anfichet", host: null},
-	{login: "cdomet-d", host: null},
-	{login: "bwisniew", host: null},
-	{login: "ibertran", host: null},
-	{login: "lrio", host: null}
+	{login: "ljurdant", location: null},
+	{login: "etessier", location: null},
+	{login: "atonkopi", location: null},
+	{login: "alsiavos", location: null},
+	{login: "pgrellie", location: null},
+	{login: "pduhamel", location: null},
+	{login: "ede-cola", location: null},
 ]
 
 async function main() {
-const users_location = await api42.getCampusLocations(9, true);
-//console.log(users_location);
-
+const usersLocation = await api42.getCampusLocations(1, true);
+usersLocation.forEach((user) => {
+	const trackUser = tracking.find(log => log.login === user.user.login);
+	if (trackUser) {
+		trackUser.location = user.host;
+	}
+	});
+tracking.forEach((trackUser) => {
+	if (trackUser.location === null) {
+		trackUser.location = "unavailable";
+	}
+	});
+	
+console.log(tracking);
 }
 
 main();
