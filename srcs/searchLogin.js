f0e75ca4d2aa345a6829ce9292a179cae2d74420
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-09-24 13:56:41 +0200

    feat: add multiple login search

--- srcs/searchLogin.js
+++ srcs/searchLogin.js
@@ -3,19 +3,21 @@ const { getProfileEmbed } = require("./getProfileEmbed.js");
 
 // Search login and display an embed message in any channel with tag <login>@42
 async function searchLogin(message) {
-	if (message.author.bot) return;
-	const index_end = message.content.search("@42");
-	if (index_end === -1 || index_end === 0) return;
-	let index = index_end - 1;
+	if (message.author.bot)
+		return;
 	message.content = message.content.toLowerCase();
-	while (index > 0 && message.content[index - 1] != " ") index--;
-	const login = message.content.slice(index, index_end);
-	try {
-		const user = await api42.getUser(login);
-		await message.reply({ embeds: [getProfileEmbed(user)] });
-	} catch (err) {
-		if (!err.rawError)
-			message.reply(`\`${login}\` : login not found`).catch(() => {});
+	let match = message.content.match(/[a-z0-9]+@42/g)
+	if (!match)
+		return;
+	for (let login of match) {
+		login = login.slice(0, login.length - 3);
+		try {
+			const user = await api42.getUser(login);
+			await message.reply({ embeds: [getProfileEmbed(user)] });
+		} catch (err) {
+			if (!err.rawError)
+				message.reply(`\`${login}\` : login not found`).catch(() => {});
+		}
 	}
 }
 
