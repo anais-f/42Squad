c94f8acbede2006b2469bf521fe6b93338a1fa57
anfichet <anfichet@student.42lyon.fr>
2025-08-28 15:58:32 +0200

    add: students DB and replace array of login by db call

--- srcs/database.js
+++ srcs/database.js
@@ -3,9 +3,27 @@ import Database from 'better-sqlite3';
 const db = new Database('./db/database.sqlite');
 db.pragma('journal_mode = WAL');
 
-
+// DB channels
 db.exec("CREATE TABLE IF NOT EXISTS channels (channelID TEXT NOT NULL UNIQUE, msgID TEXT)");
+// TODO : replace with a discord bot command to set the channelID and insert chanID via command
 db.exec("INSERT OR IGNORE INTO channels VALUES (1276305483333898291, NULL)");
-// TODO : make a discord bot command to set the channelID
+
+// DB students tracking
+db.exec("CREATE TABLE IF NOT EXISTS students (login TEXT NOT NULL UNIQUE, host TEXT)");
+// TODO : replace discord command to add and delete students from tracking
+db.exec("INSERT OR IGNORE INTO students VALUES ('acancel', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('anfichet', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('bwisniew', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('cdomet-d', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('csweetin', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('ibertran', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('kchillon', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('lcottet', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('lrio', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('mjuffard', NULL)");
+db.exec("INSERT OR IGNORE INTO students VALUES ('scros', NULL)");
+
+//DB chan/users
+// db.exec("CREATE TABLE IF NOT EXISTS chan_students (channelID TEXT NOT NULL, login TEXT NOT NULL)");
 
 export default db;
--- srcs/displayLocations.js
+++ srcs/displayLocations.js
@@ -2,42 +2,49 @@ import { EmbedBuilder } from "discord.js";
 import { api42 } from "./apiInterface.js";
 import db from "./database.js";
 
-const tracking = [
-  { login: "acancel", host: null },
-  { login: "anfichet", host: null },
-  { login: "bwisniew", host: null },
-  { login: "cdomet-d", host: null },
-  { login: "csweetin", host: null },
-  { login: "ibertran", host: null },
-  { login: "kchillon", host: null },
-  { login: "lcottet", host: null },
-  { login: "lrio", host: null },
-  { login: "mjuffard", host: null },
-  { login: "scros", host: null },
-];
+// const tracking = [
+//   { login: "acancel", host: null },
+//   { login: "anfichet", host: null },
+//   { login: "bwisniew", host: null },
+//   { login: "cdomet-d", host: null },
+//   { login: "csweetin", host: null },
+//   { login: "ibertran", host: null },
+//   { login: "kchillon", host: null },
+//   { login: "lcottet", host: null },
+//   { login: "lrio", host: null },
+//   { login: "mjuffard", host: null },
+//   { login: "scros", host: null },
+// ];
+//
+// async function trackLocation() {
+//   const usersLocation = await api42.getCampusLocations(9, true);
+//   tracking.forEach((user) => {
+//     const trackUser = usersLocation.find(
+//         (loginUser) => loginUser.user.login === user.login
+//     );
+//     if (trackUser) {
+//       user.host = trackUser.host;
+//     } else {
+//       user.host = null;
+//     }
+//   });
+//   return tracking;
+// }
 
+// on MAJ le host de la table tracking
 async function trackLocation() {
   const usersLocation = await api42.getCampusLocations(9, true);
-  tracking.forEach((user) => {
-    const trackUser = usersLocation.find(
-      (loginUser) => loginUser.user.login === user.login
-    );
-    if (trackUser) {
-      user.host = trackUser.host;
-    } else {
-      user.host = null;
-    }
-  });
-  return tracking;
+
+  const updateHost = db.prepare("UPDATE students SET host = ? WHERE login = ?");
+  usersLocation.forEach((user) => {
+    updateHost.run(user.host, user.user.login);
+  })
 }
 
-async function displayLocations(client) {
-  try {
-    let locations = await trackLocation();
-    locations = locations.filter((element) => element.host);
-    locations.sort((a, b) => a.host.localeCompare(b.host));
 
-    const embed = new EmbedBuilder()
+
+function createLocationEmbed(locations) {
+ const embed = new EmbedBuilder()
       .setColor("#00ecef")
       .setDescription("## 🪿 Currently logged in")
       .setTimestamp();
@@ -48,9 +55,23 @@ async function displayLocations(client) {
         inline: true,
       });
     });
+    return embed;
+}
 
+async function displayLocations(client) {
+  try {
+    // let locations = await trackLocation();
+    // locations = locations.filter((element) => element.host);
+
+    await trackLocation();
+    const locations = db.prepare("SELECT * FROM students WHERE host IS NOT NULL").all();
+    locations.sort((a, b) => a.host.localeCompare(b.host));
+
+    const embed = createLocationEmbed(locations);
+   
     const channelID = process.env.TRACK_CHANNEL;
     const channel = client.channels.cache.get(channelID);
+
     if (channel) {
       const lastMessage = db
         .prepare("SELECT msgID FROM channels WHERE channelID = ?")
@@ -99,5 +120,7 @@ async function displayLocations(client) {
     console.error(error);
   }
 }
+
+// async function createAndSendMessage()
 
 export default displayLocations;
