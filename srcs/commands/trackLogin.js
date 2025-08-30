a849f5fee21caee2cf4dfcbdb510fb3deb94eb29
anfichet <anfichet@student.42lyon.fr>
2025-08-30 18:00:20 +0200

    feat: trackLogin is ok

--- srcs/commands/trackLogin.js
+++ srcs/commands/trackLogin.js
@@ -2,6 +2,7 @@ import { SlashCommandBuilder } from 'discord.js';
 import { MessageFlags } from 'discord.js';
 import db from '../database.js';
 import { checkUserPermissions, checkBotPresenceAndPermissions} from "../commandsCheck.js";
+import { api42 } from "../apiInterface.js";
 
 export const data = new SlashCommandBuilder()
     .setName('tracklogin')
@@ -13,14 +14,10 @@ export const data = new SlashCommandBuilder()
           .setMaxLength(8)
     );
 
-// channelID, login
-// checker si le login existe via l'api
-// checker si le login est deja dans la db students -> si non ajouter le login
-// checker si le login est deja dans la db channels_logins pour ce channel -> si non l'ajouter
-
 export async function execute(interaction) {
   const channel = interaction.channel;
   const channelID = channel.id;
+  const login = interaction.options.getString('tracklogin').toLowerCase();
 
   try {
     const botCheck = await checkBotPresenceAndPermissions(interaction, channel);
@@ -29,11 +26,39 @@ export async function execute(interaction) {
     const userPermissionsCheck = await checkUserPermissions(interaction, channel);
     if (!userPermissionsCheck.success) return interaction.reply({ content: userPermissionsCheck.message, flags: MessageFlags.Ephemeral });
 
-    const channelInDb = await db.valueExists('channels', 'channelID', channelID)
-    if (!channelInDb) return interaction.reply({ content: channelInDb.message, flags: MessageFlags.Ephemeral });
+    // Check if channel exists in the database
+    const channelExisted = await db.valueExists('channels', 'channelID', channelID)
+    if (!channelExisted) return interaction.reply({ content: `Channel <#${channel.name}> does not exist in the database. Track channel before track login.`, flags: MessageFlags.Ephemeral });
 
-    // await db.addValue('channels', 'channelID', channelID);
-    // return interaction.reply({ content: `Channel <#${channelID}> added to the database.`, flags: MessageFlags.Ephemeral });
+    // check if login exists in the database students
+    const loginExisted = await db.valueExists('students', 'login', login)
+    if (loginExisted) {
+     // check if login is already tracked in this channel (table channels_logins)
+      const loginTrackedInChannel = db.prepare("SELECT 1 FROM tracked WHERE channelID = ? AND login = ?").get(channelID, login);
+      console.log("log: ", loginTrackedInChannel);
+        // if yes -> return error message
+      if (loginTrackedInChannel) return interaction.reply({ content: `Login ${login} is already tracked in this channel <#${channel.name}>.`, flags: MessageFlags.Ephemeral });
+        // if no -> add it to the table channels_logins
+      const stmt = db.prepare("INSERT INTO tracked (channelID, login) VALUES (?, ?)");
+      stmt.run(channelID, login);
+
+      return interaction.reply({ content: `Login \`${login}\` is now tracked in this channel <#${channel.name}>.`, flags: MessageFlags.Ephemeral });
+    }
+    else if (!loginExisted) {
+      try {
+        await api42.getUser(login);
+        const studentStmt = db.prepare("INSERT INTO students (login) VALUES (?)");
+        studentStmt.run(login);
+        const stmt = db.prepare("INSERT INTO tracked (channelID, login) VALUES (?, ?)");
+        stmt.run(channelID, login);
+        return interaction.reply({ content: `Login \`${login}\` added in the tracked channel.`, flags: MessageFlags.Ephemeral });
+      }
+      catch (error) {
+        if (!error.rawError) {
+          return interaction.reply({content: `Login \`${login}\` not found on 42 API.`, flags: MessageFlags.Ephemeral});
+        }
+      }
+    }
   }
   catch (error) {
     console.error('Error adding channel:', error);
--- srcs/database.js
+++ srcs/database.js
@@ -2,23 +2,19 @@ import Database from 'better-sqlite3';
 
 const db = new Database('./db/database.sqlite');
 db.pragma('journal_mode = DELETE');
+db.pragma('foreign_keys = ON');
 
 db.exec("CREATE TABLE IF NOT EXISTS channels (channelID TEXT NOT NULL UNIQUE, msgID TEXT)");
+db.exec("INSERT OR IGNORE INTO channels VALUES ('1276305483333898291', NULL)");
 
 // DB students tracking
 db.exec("CREATE TABLE IF NOT EXISTS students (login TEXT NOT NULL UNIQUE, host TEXT)");
 // TODO : replace discord command to add and delete students from tracking
-db.exec("INSERT OR IGNORE INTO students VALUES ('acancel', NULL)");
 db.exec("INSERT OR IGNORE INTO students VALUES ('anfichet', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('bwisniew', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('cdomet-d', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('csweetin', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('ibertran', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('kchillon', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('lcottet', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('lrio', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('mjuffard', NULL)");
-db.exec("INSERT OR IGNORE INTO students VALUES ('scros', NULL)");
+
+// DB trackLogin per channel
+db.exec("CREATE TABLE IF NOT EXISTS tracked (channelID TEXT NOT NULL, login TEXT NOT NULL, FOREIGN KEY (channelID) REFERENCES channels(channelID), FOREIGN KEY (login) REFERENCES students(login), UNIQUE(channelID, login))");
+db.exec("INSERT OR IGNORE INTO tracked VALUES ('1276305483333898291', 'anfichet')");
 
 /**
  * Check if a value exists in a specific table and column.
